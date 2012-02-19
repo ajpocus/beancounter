65874e8b199f9d48c25b074dc37d00ef81ee6e6b
Austin J. Pocus <ajpocus@gmail.com>
2012-02-18 16:07:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beancounter::Application.config.secret_token = '0ef44fedcac49e6a87d97bac3009241596067560b23f10f4b8c6ed763c226e9f17ae29290063114495da3acb5076295ae9033758217dbc2ff9d96349f4c04f50'
