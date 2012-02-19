602a07e87c6168e1dacc5b4682b378df6e7a6d19
Austin J. Pocus <ajpocus@gmail.com>
2012-02-18 23:24:42 -0800

    Set Account balance to 0 after initialization

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,16 @@
 class Account < ActiveRecord::Base
+  belongs_to :user
+
   validates_presence_of :name
   validates_uniqueness_of :name
   
   scope :by_user, lambda { |user| where(:user_id => user.id) }
+  
+  after_initialize :set_initial_balance
+  
+  private
+  
+  def set_initial_balance
+    self.balance ||= 0.0
+  end
 end
