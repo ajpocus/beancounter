3e303d0b686ce9d0fd6bc59da293f94a090bc674
Austin J. Pocus <ajpocus@gmail.com>
2012-02-18 23:20:34 -0800

    Some REST actions for accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,5 +2,23 @@ class AccountsController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    @accounts = Account.by_user(current_user).all
+  end
+  
+  def show
+    @account = Account.by_user(current_user).find_by_id(params[:id])
+  end
+  
+  def new
+    @account = Account.by_user(current_user).new
+  end
+  
+  def create
+    @account = Account.by_user(current_user).new(params[:account])
+    if @account.save
+      redirect_to root_path, :flash => { :success => "Account created!" }
+    else
+      render 'new'
+    end
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,6 @@
 class Account < ActiveRecord::Base
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
+  scope :by_user, lambda { |user| where(:user_id => user.id) }
 end
