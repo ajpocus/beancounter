25da1759af9f55bc336715ab15ba107a9ad4d4fa
Austin J. Pocus <ajpocus@gmail.com>
2012-02-19 18:35:40 -0800

    Modify Account balance on Transaction save

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,19 @@ class Transaction < ActiveRecord::Base
   acts_as_taggable
   
   accepts_nested_attributes_for :tags
+  
+  before_save :add_balance
+  before_destroy :subtract_balance
+  
+  private
+  
+  def add_balance
+    self.account.balance += self.amount
+    self.account.save
+  end
+  
+  def subtract_balance
+    self.account.balance -= self.amount
+    self.account.save
+  end
 end
